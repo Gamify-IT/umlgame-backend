6e66db24efe03b2f20a22384ff936a3576909912
Fabian <fabian.kalitzke@arcor.de>
2025-02-17 16:27:07 +0100

    initial commit

---
+++ src/main/java/de/unistuttgart/umlgamebackend/UmlGameServiceApplication.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.umlgamebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class UmlGameServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/de/unistuttgart/umlgamebackend/AppTest.java
@@ -0,0 +1,6 @@
+package de.unistuttgart.umlgamebackend;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {}
