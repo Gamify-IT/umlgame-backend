13a44e419846b4b2c1ced686727a5c36fd078596
Fabian <fabian.kalitzke@arcor.de>
2025-04-07 17:04:47 +0200

    Update UmlGameServiceApplication.java

--- src/main/java/de/unistuttgart/umlgamebackend/UmlGameServiceApplication.java
+++ src/main/java/de/unistuttgart/umlgamebackend/UmlGameServiceApplication.java
@@ -1,12 +1,15 @@
 package de.unistuttgart.umlgamebackend;
 
-/**
- * Hello world!
- *
- */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
 public class UmlGameServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(UmlGameServiceApplication.class, args);
     }
 }
